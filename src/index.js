a50ff2db16485a1c2a144b78186670689cad2591
myrna90 <myrnamares90@gmail.com>
2019-02-05 13:15:48 -0600

    hice cambios de estilo en css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 function reinicio() {
     document.getElementById("pantalla-2").style.display = "none";
     document.getElementById("pantalla-3").style.display = "none";
+    document.getElementById("pantalla-4").style.display = "none";
 };
 
 reinicio();
@@ -12,35 +13,34 @@ function display(div1, div2) {
     let divElement2 = document.getElementById(div2);
 
     //Ocultando la division del inicio
-    if (divElement.style.display == 'none') {
-        divElement.style.display = 'block';
+    if (divElement.style.display == "none") {
+        divElement.style.display = "block";
     } else {
-        divElement.style.display = 'none';
+        divElement.style.display = "none";
     }
 
     //Mostrando la division del menu
-    if (divElement2.style.display == 'none') {
-        divElement2.style.display = 'block';
+    if (divElement2.style.display == "none") {
+        divElement2.style.display = "block";
     } else {
-        divElement2.style.display = 'none';
+        divElement2.style.display = "none";
     }
 };
 
-let string = ""
-let number = ""
 
 document.getElementById("buttonCode").addEventListener("click", code);
 
 function code() {
     choosed = "buttonCode"
     display("pantalla-3")
+
 };
 
 document.getElementById("buttonDecode").addEventListener("click", deCode);
 
 function deCode() {
     choosed = "buttonDecode"
-    display("pantalla-3")
+    display("pantalla-4")
 };
 
 document.getElementById("buttonHelp").addEventListener("click", help);
